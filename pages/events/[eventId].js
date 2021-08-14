bf0f42a478ab2e2584807f9cea2094b1cf633ef5
harshmalviya <harshmalviya72@gmail.com>
2021-08-14 13:48:06 +0530

    first commit

---
+++ pages/events/[eventId].js
@@ -0,0 +1,40 @@
+import { useRouter } from "next/dist/client/router";
+import { Fragment } from "react";
+import { getEventById } from "../../DUMMY_EVENTS";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+import Button from "../../components/ui/Button";
+
+function EventDetail() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <Fragment>
+        <ErrorAlert>No event found.</ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+export default EventDetail;
---
+++ pages/events/index.js
@@ -0,0 +1,20 @@
+import { getAllEvents } from "../../DUMMY_EVENTS";
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+import { Fragment } from "react";
+import { useRouter } from "next/dist/client/router";
+function AllEventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+export default AllEventsPage;
